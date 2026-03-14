0209de9db83035fdfd2f4b6aac4974fe5e05780f
Svasilev22 <kamiash9247@gmail.com>
2026-03-14 17:09:43 +0200

    Update CommandInterpreter.java

--- CommandInterpreter.java
+++ CommandInterpreter.java
@@ -17,12 +17,15 @@ public class CommandInterpreter {
     }
 
     public void start() {
+
         System.out.println("DTM System started.");
         System.out.println("Type 'help' to see available commands.");
 
         while (running) {
+
             System.out.print("> ");
             String input = scanner.nextLine();
+
             processCommand(input);
         }
     }
@@ -48,18 +51,22 @@ public class CommandInterpreter {
                 break;
 
             case "newTM":
+
                 if (parts.length < 2) {
                     System.out.println("Usage: newTM <name>");
                 } else {
                     manager.createMachine(parts[1]);
                 }
+
                 break;
 
             case "list":
+
                 manager.listMachines();
                 break;
 
             case "print":
+
                 if (parts.length < 2) {
                     System.out.println("Usage: print <id>");
                 } else {
@@ -70,6 +77,67 @@ public class CommandInterpreter {
                         System.out.println("Invalid ID.");
                     }
                 }
+
+                break;
+
+            case "addState":
+
+                if (parts.length < 3) {
+                    System.out.println("Usage: addState <id> <state>");
+                } else {
+                    try {
+                        int id = Integer.parseInt(parts[1]);
+                        manager.addState(id, parts[2]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid ID.");
+                    }
+                }
+
+                break;
+
+            case "setStart":
+
+                if (parts.length < 3) {
+                    System.out.println("Usage: setStart <id> <state>");
+                } else {
+                    try {
+                        int id = Integer.parseInt(parts[1]);
+                        manager.setStart(id, parts[2]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid ID.");
+                    }
+                }
+
+                break;
+
+            case "addAccept":
+
+                if (parts.length < 3) {
+                    System.out.println("Usage: addAccept <id> <state>");
+                } else {
+                    try {
+                        int id = Integer.parseInt(parts[1]);
+                        manager.addAccept(id, parts[2]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid ID.");
+                    }
+                }
+
+                break;
+
+            case "addReject":
+
+                if (parts.length < 3) {
+                    System.out.println("Usage: addReject <id> <state>");
+                } else {
+                    try {
+                        int id = Integer.parseInt(parts[1]);
+                        manager.addReject(id, parts[2]);
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid ID.");
+                    }
+                }
+
                 break;
 
             default:
@@ -78,11 +146,18 @@ public class CommandInterpreter {
     }
 
     private void printHelp() {
+
         System.out.println("Available commands:");
-        System.out.println("  newTM <name>   - create new Turing Machine");
-        System.out.println("  list           - list all machines");
-        System.out.println("  print <id>     - print machine info");
-        System.out.println("  help           - show commands");
-        System.out.println("  exit           - exit program");
+        System.out.println("  newTM <name>        - create new Turing Machine");
+        System.out.println("  list                - list all machines");
+        System.out.println("  print <id>          - print machine information");
+
+        System.out.println("  addState <id> <s>   - add state");
+        System.out.println("  setStart <id> <s>   - set start state");
+        System.out.println("  addAccept <id> <s>  - add accept state");
+        System.out.println("  addReject <id> <s>  - add reject state");
+
+        System.out.println("  help                - show commands");
+        System.out.println("  exit                - exit program");
     }
 }
