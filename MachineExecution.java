abf64ee145f07203459e265f51d8cc0ae74b878f
Svasilev22 <kamiash9247@gmail.com>
2026-04-19 17:18:24 +0300

    MachineExecution.java

--- MachineExecution.java
+++ MachineExecution.java
@@ -11,30 +11,31 @@ public class MachineExecution {
 
     private String currentState;
     private boolean halted;
+    private int steps;
 
     public MachineExecution(TuringMachine machine, Tape tape) {
         this.machine = machine;
         this.tape = tape;
         this.halted = false;
+        this.steps = 0;
     }
 
     public void init(String input) {
 
         tape.reset();
 
-        // записваме входа на лентата
         for (int i = 0; i < input.length(); i++) {
             tape.write(input.charAt(i));
             tape.move(core.Move.R);
         }
 
-        // връщаме главата в началото
         for (int i = 0; i < input.length(); i++) {
             tape.move(core.Move.L);
         }
 
         currentState = machine.getStartState();
         halted = false;
+        steps = 0;
 
         System.out.println("Execution initialized.");
     }
@@ -60,6 +61,7 @@ public class MachineExecution {
         tape.move(t.getMove());
 
         currentState = t.getToState();
+        steps++;
 
         if (machine.isAcceptState(currentState)) {
             halted = true;
@@ -70,9 +72,21 @@ public class MachineExecution {
         }
     }
 
+    public void run(int maxSteps) {
+
+        while (!halted && steps < maxSteps) {
+            step();
+        }
+
+        if (!halted) {
+            System.out.println("Max steps reached.");
+        }
+    }
+
     public void printStatus() {
         System.out.println("Current state: " + currentState);
         System.out.println("Halted: " + halted);
+        System.out.println("Steps: " + steps);
     }
 
     public boolean isHalted() {
